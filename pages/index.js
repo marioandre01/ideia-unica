e1c4aa755d00e147f5607a7cab52e27d7722a514
Mario Andre <mario_andre.l.a@hotmail.com>
2020-11-15 18:49:37 -0300

    nova chamada na home

--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,7 @@
 function Home() {
     return (
         <div>
-            <h1>Home</h1>
+            <h1>Página inicial</h1>
             {/*<h2>Home</h2>*/}
             {/*<Contador />*/}
         </div>
